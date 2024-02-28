7e554b16ff0e465c0df5dd6aa388cf33081a753c
zlb <2154167841@qq.com>
2024-02-28 20:22:30 +0800

    shader version 1

--- model.h
+++ model.h
@@ -1,30 +1,32 @@
 #ifndef __MODEL_H__
 #define __MODEL_H__
-
 #include <vector>
+#include <string>
 #include "geometry.h"
-
-
-// 存储faces信息(vertex , uv , normal)
-struct Face {
-    std::vector<int> vertexIndices;
-    std::vector<int> texcoordIndices;
-    //normal
-};
+#include "tgaimage.h"
 
 class Model {
 private:
     std::vector<Vec3f> verts_;
-    std::vector<Face> faces_; // 每个obj都有三个元素
-    std::vector<Vec2f> texcoords_;
+    std::vector<std::vector<Vec3i> > faces_; // attention, this Vec3i means vertex/uv/normal
+    std::vector<Vec3f> norms_;
+    std::vector<Vec2f> uv_;
+    TGAImage diffusemap_;
+    TGAImage normalmap_;
+    TGAImage specularmap_;
+    void load_texture(std::string filename, const char *suffix, TGAImage &img);
 public:
     Model(const char *filename);
     ~Model();
     int nverts();
     int nfaces();
+    Vec3f normal(int iface, int nthvert);
+    Vec3f normal(Vec2f uv);
     Vec3f vert(int i);
-    Face face(int idx);
-    Vec2f& getTexCoord(int index);
+    Vec3f vert(int iface, int nthvert);
+    Vec2f uv(int iface, int nthvert);
+    TGAColor diffuse(Vec2f uv);
+    float specular(Vec2f uv);
+    std::vector<int> face(int idx);
 };
-
 #endif //__MODEL_H__
